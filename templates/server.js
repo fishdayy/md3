ecee714fa1b96d45d3cbe497e88b556e5c041ea9
Tuấn Anh <110382884+fishdayy@users.noreply.github.com>
2022-10-05 11:06:41 +0700

    done

---
+++ templates/server.js
@@ -0,0 +1,22 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const fs = require("fs");
+
+const httpServer = createServer(function (req, res) {
+    fs.readFile('./templates/index.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+});
+
+const io = new Server(httpServer);
+
+/* lắng nghe kết nối socket từ phía client */
+io.on("connection", (socket) => {
+    console.log('a user connected')
+});
+
+httpServer.listen(3000, 'localhost', function (){
+    console.log('listening on http://localhost:3000');
+});
