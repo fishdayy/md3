e12f1ff382b336de5b296936cd56959df5a65308
Tuấn Anh <110382884+fishdayy@users.noreply.github.com>
2022-10-07 10:46:06 +0700

    done

---
+++ webHTTP/login/app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+http.createServer(function (req, res){
+    let txt = ""
+    if (req.url === "/login"){
+        txt = "login success";
+    }else {
+        txt = "login fail"
+    }
+    res.end(txt);
+}).listen(8080,"localhost");
