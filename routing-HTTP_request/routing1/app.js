8e96bf51e5aa048ea99630eea88305b3ab0d3c80
Tuấn Anh <110382884+fishdayy@users.noreply.github.com>
2022-10-11 14:04:55 +0700

    done

---
+++ routing-HTTP_request/routing1/app.js
@@ -0,0 +1,57 @@
+const http = require('http');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+
+let server = http.createServer(function (req, res) {
+
+    let handlers = {};
+
+    handlers.sample = function (data, callback) {
+        callback(406, {'name': 'sample handle'})
+    };
+
+    handlers.notFound = function (data, callback) {
+        callback(404);
+    };
+
+    handlers.home = function (data, callback) {
+
+        callback(200, 'home page');
+    };
+
+    let router = {
+        'sample': handlers.sample,
+        'home': handlers.home,
+    }
+
+    let parseUrl = url.parse(req.url, true);
+    let path = parseUrl.pathname;
+    let trimPath = path.replace(/^\/+|\/+$/g, '');
+    req.on('data', function (data) {
+    });
+    req.on('end', function (end) {
+        let chosenHandler = (typeof (router[trimPath]) !== 'undefined') ? router[trimPath] : handlers.notFound;
+        let data=
+            {
+                "trimPath": trimPath
+            }
+        ;
+
+        chosenHandler(data, function (statusCode, payload) {
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+            payload = typeof (payload) == 'object' ? payload : {};
+            let payLoadString = JSON.stringify(payload);
+            res.writeHead(statusCode)
+            res.end(payLoadString);
+            //log the request
+            console.log("status "+ statusCode + "payload" + payload);
+        });
+
+    });
+
+    res.end();
+})
+//server start
+server.listen(3000, function () {
+    console.log("the server is listening on port 3000 now ");
+})
